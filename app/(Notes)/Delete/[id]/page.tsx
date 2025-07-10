c51393cd3ac2b3580013ab666c803da1688c00a7
Atharva Karemore <karemoreatharva@gmail.com>
2025-07-10 14:08:45 +0530

    change

--- app/(Notes)/Delete/[id]/page.tsx
+++ app/(Notes)/Delete/[id]/page.tsx
@@ -1,5 +1,13 @@
 // app/(Notes)/Delete/[id]/page.tsx
+
 import SingleNoteView from "@/components/SingleNoteView";
+
+// Define type for route params
+type PageProps = {
+  params: {
+    id: string;
+  };
+};
 
 async function getNote(id: string) {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/show/${id}`, {
@@ -13,11 +21,7 @@ async function getNote(id: string) {
   return res.json();
 }
 
-export default async function Page({
-  params,
-}: {
-  params: { id: string };
-}) {
+export default async function Page({ params }: any) {
   const note = await getNote(params.id);
 
   return (
