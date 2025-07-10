dcffa7c2d3eb2a051c24dbcb5675ebfe1cab7cf2
Atharva12121 <154357433+Atharva12121@users.noreply.github.com>
2025-07-11 02:58:07 +0530

    Update NoteWriter.tsx

--- components/NoteWriter.tsx
+++ components/NoteWriter.tsx
@@ -176,7 +176,7 @@ const handleGenerateFromPDF = async () => {
 
     setLoading(true);
     setErrors({});
-
+// cccccc
     try {
       const response = await fetch("https://ai-notes-backend-ghj3.onrender.com/Addnotes", {
       
